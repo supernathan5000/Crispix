74131462a31dc45eb3b02c539c3c698dada1fb0e
Nathan Porter <nathanporter1@gmail.com>
2017-07-15 19:38:18 -0500

    Commit of moved Standings controller

---
+++ crispix2.0/Controllers/StandingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crispix2._0.Models.Standings;
+
+namespace crispix2._0.Controllers
+{
+    public class StandingsController : Controller
+    {
+        // GET: Standings
+        public ActionResult Standings()
+        {
+            StandingsLoad standings = new StandingsLoad();
+
+            GetStandings(standings);
+
+            return View(standings);
+        }
+
+        private void GetStandings(StandingsLoad standings)
+        {
+            //build the query to get the standings
+
+            //foreach result create a StandingsRow and add it to the standings.StandingsList
+        }
+    }
+}
